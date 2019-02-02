fc7ebf1cb918e8d61876346abff52ec4e3ccef63
Gu-Miao <1450206741@qq.com>
2019-02-02 16:58:20 +0800

    add start script; change structure of catalog

---
+++ doc/design/a.js
@@ -0,0 +1,30 @@
+var a = {
+    "answer": "",
+    "belong": "",
+    "content": "",
+    "createTime": null,
+    "id": "",
+    "keywords": "",
+    "lastModifiedTime": null,
+    "option": {
+        "questionOptionLength": 0,
+        "questionOptions": [
+            {
+                "content": "",
+                "isRight": false,
+                "order": 0
+            }
+        ]
+    },
+    "smallQuestion": {
+        "samllQuestionLength": 0,
+        "smallQuestions": [
+            {
+                "content": "",
+                "score": 0
+            }
+        ]
+    },
+    "score": 0,
+    "type": ""
+}
