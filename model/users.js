8678d7e72d215376e90050fd9d9b4910827a1c66
Gu-Miao <1450206741@qq.com>
2019-04-23 10:51:28 +0800

    update b4-table library

---
+++ model/users.js
@@ -0,0 +1,46 @@
+// 引入 Mongoose
+let mongoose = require('./connect');
+
+// 定义学生集合数据结构
+let usersSchema = mongoose.Schema({
+    name: { // 中文姓名
+        type: String,
+        trim: true,
+        minlength: 2,
+        maxlength: 20
+    },
+    uid: { // 学工号
+        type: String,
+        trim: true,
+        minlength: 1
+    },
+    grade: { // 年级
+        type: String,
+        trim: true,
+        minlength: 1
+    },
+    college: { // 所属学院
+        type: String,
+        trim: true,
+        minlength: 1
+    },
+    major: { // 所属专业
+        type: String,
+        trim: true,
+        minlength: 1
+    },
+    username: { // 登录账号
+        type: String,
+        trim: true,
+        minlength: 1
+    },
+    password: { // 登录密码，6~16 位数字字母，符号只可以使用 “_” 和 “.”。
+        type: String,
+        trim: true,
+        minlength: 6,
+        maxlength: 16,
+        match: /^[\w_.]{6,16}$/
+    }
+});
+
+module.exports = mongoose.model('Users', usersSchema, 'users');
